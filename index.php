e96375c9c599a2bbed12df2353037bd4bb0bd03f
Tarek Hamze <trkhmz503@gmail.com>
2023-12-22 15:42:33 +0200

    2nd commit

--- index.php
+++ index.php
@@ -119,21 +119,9 @@
                 <div class="col-lg-12">
                     <nav class="header__menu mobile-menu">
                         <ul>
-                            <li class="active"><a href="./index.html">Home</a></li>
-                            <li><a href="./about.html">About</a></li>
-                            <li><a href="./shop.html">Shop</a></li>
-                            <li><a href="#">Pages</a>
-                                <ul class="dropdown">
-                                    <li><a href="./shop-details.html">Shop Details</a></li>
-                                    <li><a href="./shoping-cart.html">Shoping Cart</a></li>
-                                    <li><a href="./checkout.html">Check Out</a></li>
-                                    <li><a href="./wisslist.html">Wisslist</a></li>
-                                    <li><a href="./Class.html">Class</a></li>
-                                    <li><a href="./blog-details.html">Blog Details</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="./blog.html">Admin Dashboard</a></li>
-                            <li><a href="./contact.html">Contact</a></li>
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="admin-dashboard.html">Admin Dashboard</a></li>
+                            <li><a href="contact.html">Contact</a></li>
                         </ul>
                     </nav>
                 </div>
